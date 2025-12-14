1ed2f439b9358b04d51581341c1a1a6370cb4709
orfeus <xrafaelzahir@gmail.com>
2025-12-13 20:47:10 -0400

    📋 Finalizando primeira versão do site

---
+++ js/contactme.js
@@ -0,0 +1,30 @@
+// 1. Encontramos o formulário pelo ID
+document.addEventListener("DOMContentLoaded", function () {
+
+    const _form = document.getElementById("contactForm");
+
+    _form.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const _name = document.getElementById("name").value;
+    const _email = document.getElementById("email").value;
+    const _message = document.getElementById("message").value;
+
+    const _path = window.location.pathname;
+    const _isEnglish = _path.startsWith("/en");
+
+    const _subjectText = _isEnglish ? "Contact via portfolio website" : "Contato pelo site";
+
+    const _subject = encodeURIComponent(_subjectText);
+
+    const _body = encodeURIComponent(
+        `Nome: ${_name}\n` +
+        `Email: ${_email}\n\n` +
+        `Mensagem:\n${_message}`
+    );
+
+    window.location.href =
+        `mailto:orfeu.dev@gmail.com?subject=${_subject}&body=${_body}`;
+    });
+});
